053ec95eeff22845df0c92360820e435b4c6a0da
Яна Попова <ynappva@gmail.com>
2024-04-24 12:31:59 +0300

    BonusMilesService updated

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,9 +2,7 @@ public class BonusMilesService {
 
     public int calculate(int price) {
         int miles = price / 20;
-        if (miles > 500) {
-            miles = 500;
-        }
+
         return miles;
     }
 }
